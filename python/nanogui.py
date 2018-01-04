f19e8628832ecf5f084db8376d81879bb2711c9a
Andrew Davison <davisoa@gmail.com>
2018-01-04 16:48:06 -0500

    Update CMakeLists.txt to make a catkin package, and also put the python libraries in catkin's python module directory

---
+++ python/nanogui.py
@@ -0,0 +1,8 @@
+# From: https://stackoverflow.com/questions/10968309/how-to-import-python-module-from-so-file
+def __bootstrap__():
+   global __bootstrap__, __loader__, __file__
+   import sys, pkg_resources, imp
+   __file__ = pkg_resources.resource_filename(__name__,'nanogui.so')
+   __loader__ = None; del __bootstrap__, __loader__
+   imp.load_dynamic(__name__,__file__)
+__bootstrap__()
